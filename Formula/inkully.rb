bedadd9d9035ebc7463549b7e39026a98cb66b73
sudoshmudo <sudoshmudo>
2026-04-10 18:36:46 +0200

    Revert "add GitHubPrivateRepositoryReleaseDownloadStrategy"
    This reverts commit 96b1307f4a71e8a65295ee54421372855d719af6.

--- Formula/inkully.rb
+++ Formula/inkully.rb
@@ -5,7 +5,7 @@ class Inkully < Formula
   # macOS ARM64 bottle
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sudoshmudo/inkully/releases/download/v0.1.0/inkully-osx-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/sudoshmudo/inkully/releases/download/v0.1.0/inkully-osx-arm64.tar.gz"
       sha256 "f57df469998b8da1289805210e7c1673ae6e2779d8f5376bc8296ddd43d54c3e"
     else
       odie "Only ARM64 macOS is supported"
@@ -15,7 +15,7 @@ class Inkully < Formula
   # Linux ARM64 bottle
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sudoshmudo/inkully/releases/download/v0.1.0/inkully-linux-arm64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+      url "https://github.com/sudoshmudo/inkully/releases/download/v0.1.0/inkully-linux-arm64.tar.gz"
       sha256 "db775cbda02302af9b2f5464c266410f41effaa989982fbbae2c5b8522d5b740"
     else
       odie "Only ARM64 Linux is supported"
